4790f7759c13d00a9b8017ef5232bc0b4cd92d14
Quanlong He <kyanhe@cienet.com.cn>
2014-05-22 17:49:56 +0800

    Update to 0.7.2.

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from distutils.core import setup
 
 setup(
     name='gclient',
-    version='0.7',
+    version='0.7.2',
     author='Quanlong He',
     author_email='kyan.ql.he@gmail.com',
     scripts=['bin/gclient'],
-    packages=['depot_tools', 'depot_tools.third_party.colorama', 'depot_tools.third_party.repo'],
+    packages=[
+        'depot_tools',
+        'depot_tools.third_party',
+        'depot_tools.third_party.colorama',
+        'depot_tools.third_party.repo'],
     url='https://github.com/cybertk/gclient',
     license='BSD-style License, see LICENSE.txt',
     classifiers=[
